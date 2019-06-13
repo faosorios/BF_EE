c9529a432de8cbaf646109b2f7358dfc064f1322
Felipe Osorio <faosorios@gmail.com>
2019-06-13 14:23:26 -0400

    Add files via upload

---
+++ code/BF.simul.R
@@ -0,0 +1,144 @@
+## Id: BF.simul.R, last updated 2019/06/05
+## Author: originally coded by Federico Crudu, with contributions of Felipe Osorio
+
+simul.EE <- function(Nsize = 5000, nobs = 500, coef, sigma, alpha = 0.05)
+{ ## function to perform the simulation experiment (Section 3 of the manuscript)
+  constr.A <- function(coef) {
+    coef[2] - 1.0 / coef[3]
+  }
+  constr.B <- function(coef) {
+    coef[2] * coef[3] - 1.0
+  }
+  deriv.A <- function(coef) {
+    c(0.0, 1.0, 1.0 / coef[3]^2)
+  }
+  deriv.B <- function(coef) {
+    c(0.0, coef[3], coef[2])
+  }
+  moments <- function(coef, ...) {
+    res <- y - coef[1] - coef[2] * x1 - exp(coef[3] * x2)
+    g2 <- res * x1
+    g3 <- res * exp(coef[3] * x2) * x2
+    cbind(res, g2, g3)
+  }
+  moments.deriv <- function(coef, ...) {
+    res <- y - coef[1] - coef[2] * x1 - exp(coef[3] * x2)
+    g11 <- -1
+    g12 <- -x1
+    g13 <- -exp(coef[3] * x2) * x2
+    G1  <- c(sum(g11), sum(g12), sum(g13))
+    g21 <- g11 * x1
+    g22 <- g12 * x1
+    g23 <- g13 * x1
+    G2  <- c(sum(g21), sum(g22), sum(g23))
+    g31 <- g11 * exp(coef[3] * x2) * x2
+    g32 <- g12 * exp(coef[3] * x2) * x2
+    g33 <- g13 * exp(coef[3] * x2) * x2 + res * exp(coef[3] * x2) * x2 * x2
+    G3  <- c(sum(g31), sum(g32), sum(g33))
+    cbind(G1, G2, G3)
+  }
+  c.objective <- function(coef) {
+    res <- y - coef[1] - coef[2] * x1 - exp(coef[3] * x2)
+    g2 <- res * x1
+    g3 <- res * exp(coef[3] * x2) * x2
+    g  <- cbind(res, g2, g3)
+    crossprod(colSums(g))
+  }
+
+  ok <- matrix(FALSE, nrow = Nsize, ncol = 7) # results container
+  now <- proc.time()
+
+  cat("  Progress:\n")
+  pb <- txtProgressBar(min = 0, max = Nsize, style = 3)
+
+  # Monte Carlo iterations
+  for (i in 1:Nsize) {
+    set.seed(123 + i)
+
+    # model building
+    coef.true <- coef
+    x1  <- rnorm(nobs, 0, sigma)
+    x2  <- rnorm(nobs, 0, sigma)
+    eps <- rnorm(nobs, 0, sigma)
+    y <- coef.true[1] + coef.true[2] * x1 + exp(coef.true[3] * x2) + eps
+
+    # fitting unconstrained model
+    X <- cbind(y,x1,x2)
+    fm0 <- gmm(moments, X, coef.true)
+    cf0 <- fm0$coefficients
+    res0 <-fm0$residuals
+    V0 <- fm0$vcov
+
+    # fitting unconstrained models
+    fm1 <- auglag(par = coef.true, fn = c.objective, heq = constr.A, control.outer = list(trace = FALSE))
+    cf1 <- fm1$par
+    fm2 <- auglag(par = coef.true, fn = c.objective, heq = constr.B, control.outer = list(trace = FALSE))
+    cf2 <- fm2$par
+
+    # distance metric statistic
+    g0 <- moments(cf0)
+    g1 <- moments(cf1)
+    J.hat   <- crossprod(colSums(g0), solve(crossprod(g0), colSums(g0)))
+    J.tilde <- crossprod(colSums(g1), solve(crossprod(g1), colSums(g1)))
+    DM <- J.tilde - J.hat
+
+    # Wald, BF and LM statistics (hypothesis A)
+    g1 <- moments(cf1)
+    G1 <- moments.deriv(cf1)
+    a1 <- constr.A(cf0)
+    a1.dot <- deriv.A(cf0)
+    Wald.A <- crossprod(a1, solve(crossprod(a1.dot, V0 %*% a1.dot), a1))
+    LM.A <- crossprod(colSums(g1), solve(crossprod(g1), colSums(g1)))
+    BF.A <- crossprod(G1 %*% (cf1 - cf0), solve(crossprod(g1), colSums(g1)))
+
+    # Wald, BF and LM statistics (hypothesis B)
+    g2 <- moments(cf2)
+    G2 <- moments.deriv(cf2)
+    a2 <- constr.B(cf0)
+    a2.dot <- deriv.B(cf0)
+    Wald.B <- crossprod(a2, solve(crossprod(a2.dot, V0 %*% a2.dot), a2))
+    LM.B <- crossprod(colSums(g2), solve(crossprod(g2), colSums(g2)))
+    BF.B <- crossprod(G2 %*% (cf2 - cf0), solve(crossprod(g2), colSums(g2)))
+
+    # saving results
+    df <- length(a1)
+    cutoff.A <- qchisq(1 - alpha, df)
+    cutoff.B <- qchisq(1 - alpha, 1)
+
+    ok[i,1] <- Wald.A > cutoff.A
+    ok[i,2] <- Wald.B > cutoff.B
+    ok[i,3] <- BF.A > cutoff.A
+    ok[i,4] <- BF.B > cutoff.B
+    ok[i,5] <- LM.A > cutoff.A
+    ok[i,6] <- LM.B > cutoff.B
+    ok[i,7] <- DM > cutoff.B
+
+    # update progress bar
+    setTxtProgressBar(pb, i)
+  }
+  close(pb)
+
+  tnames <- c("Wald.A","Wald.B","BF.A","BF.B","LM.A","LM.B","D")
+  percentage <- apply(ok, 2, sum) / Nsize
+  names(percentage) <- tnames
+  cutoff <- c(cutoff.A, cutoff.B)
+  names(cutoff) <- c("A","B")
+  speed <- proc.time() - now
+  out <- list(percentage = 100 * percentage, cutoffs = cutoff, speed = speed)
+  out
+}
+
+summary.EE <- function(Nsize = 5000, coef, sigma, alpha = 0.05) {
+  out <- matrix(0, nrow = 4, ncol = 7)
+  now <- proc.time()
+
+  out[1,] <- simul.EE(Nsize, nobs = 20,  coef, sigma, alpha)$percentage
+  out[2,] <- simul.EE(Nsize, nobs = 50,  coef, sigma, alpha)$percentage
+  out[3,] <- simul.EE(Nsize, nobs = 100, coef, sigma, alpha)$percentage
+  out[4,] <- simul.EE(Nsize, nobs = 500, coef, sigma, alpha)$percentage
+
+  rownames(out) <- c("20","50","100","500")
+  colnames(out) <- c("Wald.A","Wald.B","BF.A","BF.B","LM.A","LM.B","D")
+
+  list(output = out, speed = proc.time() - now)
+}
---
+++ code/simulation.R
@@ -0,0 +1,39 @@
+## These simulations were performed on an HP Proliant DL360 Server equipped with
+## with Intel Xeon E5-2630 processor at 2.4 GHz and 16 GB of RAM DDR4.
+## The total simulation time was 4 hours, 18 minutes, and 17 seconds.
+
+## reading R sources
+source("BF.simul.R")
+
+## loading 'alabama' and 'gmm' packages (required to constrained/unconstrained optimization)
+library(alabama)
+library(gmm)
+
+## creating Table 1... (it will take long time)
+
+out1 <- summary.EE(Nsize = 5000, coef = c(1,10,0.1), sigma = 0.4, alpha = 0.05)
+out2 <- summary.EE(Nsize = 5000, coef = c(1,5.,0.2), sigma = 0.4, alpha = 0.05)
+out3 <- summary.EE(Nsize = 5000, coef = c(1,2.,0.5), sigma = 0.4, alpha = 0.05)
+out4 <- summary.EE(Nsize = 5000, coef = c(1.,1.,1.), sigma = 0.4, alpha = 0.05)
+
+table1 <- rbind(out1$output, out2$output, out3$output, out4$output)
+
+print(table1 / 100, digits = 2)
+# Output:
+#    Wald.A Wald.B  BF.A  BF.B  LM.A  LM.B     D
+#20   0.420  0.176 0.067 0.064 0.087 0.084 0.087
+#50   0.282  0.106 0.065 0.059 0.074 0.068 0.074
+#100  0.197  0.077 0.059 0.059 0.061 0.061 0.061
+#500  0.104  0.052 0.048 0.048 0.049 0.049 0.049
+#20   0.277  0.178 0.068 0.065 0.086 0.083 0.086
+#50   0.171  0.108 0.058 0.057 0.068 0.067 0.068
+#100  0.127  0.078 0.058 0.058 0.062 0.061 0.062
+#500  0.070  0.052 0.047 0.047 0.048 0.048 0.048
+#20   0.145  0.175 0.066 0.067 0.082 0.082 0.082
+#50   0.096  0.113 0.062 0.057 0.075 0.070 0.075
+#100  0.078  0.082 0.056 0.056 0.062 0.062 0.062
+#500  0.049  0.055 0.045 0.045 0.050 0.050 0.050
+#20   0.140  0.170 0.084 0.070 0.101 0.086 0.101
+#50   0.095  0.108 0.062 0.062 0.070 0.070 0.070
+#100  0.074  0.080 0.066 0.066 0.067 0.067 0.067
+#500  0.055  0.055 0.056 0.056 0.061 0.060 0.061
